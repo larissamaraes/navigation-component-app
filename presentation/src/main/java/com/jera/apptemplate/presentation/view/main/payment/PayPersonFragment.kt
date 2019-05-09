545e32a0c6365e98e2d4618f958e2740fd4963cb
Larissa Marães <larissa.maraes@jera.com.br>
2019-05-08 21:03:55 -0400

    setting actions on pay person fragment

--- presentation/src/main/java/com/jera/apptemplate/presentation/view/main/payment/PayPersonFragment.kt
+++ presentation/src/main/java/com/jera/apptemplate/presentation/view/main/payment/PayPersonFragment.kt
@@ -1,9 +1,9 @@
 package com.jera.apptemplate.presentation.view.main.payment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.navigation.fragment.findNavController
+import com.jera.apptemplate.R
 import com.jera.apptemplate.databinding.FragmentPayPersonBinding
 import com.jera.apptemplate.util.base.BaseFragment
 
@@ -13,6 +13,27 @@ class PayPersonFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentPayPersonBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
+        setupUi()
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.pay_person_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.next_button) {
+            findNavController().navigate(R.id.action_payPersonFragment_to_paymentCompleteFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupUi() {
+        binding.disclaimerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_payPersonFragment_to_disclaimerFragment)
+        }
     }
 }
